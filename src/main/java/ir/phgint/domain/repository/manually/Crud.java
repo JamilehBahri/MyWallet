682898c33862bea27dd6dc75936bd2ef37b773a6
JamilehBahri <j.bahri@phgint.ir>
2020-04-25 17:37:20 +0430

    tests

--- src/main/java/ir/phgint/domain/repository/manually/Crud.java
+++ src/main/java/ir/phgint/domain/repository/manually/Crud.java
@@ -3,4 +3,7 @@ package ir.phgint.domain.repository.manually;
 public class Crud {
 
     // for manually operation (insted of jpa)
+    // for manually operation (insted of jpa)
+    // for manually operation (insted of jpa)
+
 }
